b7f17f56bd3ea5a0fd945ce9359740ff6d19cfe9
Tahashaikh <taha.tariq.shaikh@gmail.com>
2024-01-09 12:05:42 +0500

    Day 17 deleted

--- Day-20/Snake_Game.py
+++ Day-20/Snake_Game.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 is_race_on = False
 screen = Screen()
@@ -8,27 +9,20 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("My Snake Game")
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_snake = Turtle(shape="square")
-    new_snake.color("white")
-    new_snake.penup()
-    new_snake.goto(position)
-    segments.append(new_snake)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
-    for seg in segments:
-        seg.forward(20)
+    time.sleep(0.1)
 
-
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
+
